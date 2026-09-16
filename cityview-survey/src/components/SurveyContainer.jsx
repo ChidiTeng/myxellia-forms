7c249e67b11c188069550c2fa0fd7920e8c9a863
Emmanuel Nwachukwu <egreatness1@gmail.com>
2026-09-16 03:07:54 +0100

    expose response

--- cityview-survey/src/components/SurveyContainer.jsx
+++ cityview-survey/src/components/SurveyContainer.jsx
@@ -39,7 +39,6 @@ export default function SurveyContainer() {
   async function handleFinalSubmit() {
     const results = data?.results || [];
     const currentSurvey = results[0];
-    console.log('currentSurvey', currentSurvey);
 
     const recipientId =
       currentSurvey?.recipient_id ??
